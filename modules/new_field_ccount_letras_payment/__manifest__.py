9fa1cb57fdef9f4101125a535f2d08aa69010b27
Rodrigo215 <rodrigo.duenas@tecsup.edu.pe>
2020-06-23 15:35:57 -0500

    Nuevos campos letras payment

---
+++ modules/new_field_ccount_letras_payment/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': u'Codigo unico de Banco',
+	'category': 'reports',
+	'author': 'ITGRUPO-ABL-INTERNACIONAL',
+	'depends': ['account_letras_it'],
+	'version': '1.0.0',
+	'description':"""
+	Agrega un (Codigo unico de Banco) en el tree dentro de canje de letras.
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+		'views/account_letras_payment_view.xml',
+		],
+	'installable': True
+}
---
+++ modules/new_field_ccount_letras_payment/models/account_letras_payment.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class AccountLetrasPaymentManual(models.Model):
+    _inherit = 'account.letras.payment.manual'
+
+    cu_banco = fields.Char('CU Banco')
