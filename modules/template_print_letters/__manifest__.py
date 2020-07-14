266003f93626c672158e3363ccceedbd8a1d40d8
Rodrigo215 <rodrigo.duenas@tecsup.edu.pe>
2020-07-14 17:56:10 -0500

    Plamtilla de impresión Toscana

---
+++ modules/template_print_letters/__manifest__.py
@@ -0,0 +1,23 @@
+{
+	"name": "Template-Imprimir Letras",
+	"version": "1.0",
+	"depends": [
+		"vit_dotmatrix",
+		"account_letras_it",
+		"account_base_it",
+		"print_letters"
+	],
+	"author": "ITSOLUTIONS-TOSCANA",
+	'website': 'https://itgrupo-solutions.com/',
+	"description": """
+		-Función: Esta solo es una plantilla que se imprimira las letras en la impresora matricial de la empresa Toscana, ya que se ajusta a sus medidas de impresion.
+		-Desarrollador: Rodrigo Dueñas,
+		-Funcional: Jorge Salinas. 
+	""",
+	"data": [
+		"data/templates.xml"
+	],
+	"installable": True,
+	"auto_install": False,
+	"application": True,
+}
