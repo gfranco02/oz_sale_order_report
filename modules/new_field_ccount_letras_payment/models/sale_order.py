6db09cd7764afb735fd23dcef9c1db7e3183674b
Rodrigo215 <rodrigo.duenas@tecsup.edu.pe>
2020-07-24 13:35:26 -0500

    Cambio de tipo de campo

--- modules/new_field_ccount_letras_payment/models/sale_order.py
+++ modules/new_field_ccount_letras_payment/models/sale_order.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
 	_inherit = 'sale.order'
 
-	delivery_agency = fields.Many2one('res.partner',u'Agencia de envío')
+	delivery_agency = fields.Char(u'Agencia de envío')
