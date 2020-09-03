822fb511db32ce9f41e1249fdd424a480a1e9c09
Leo de Oz <astaroth.satania@gmail.com>
2020-09-03 18:46:16 -0500

    [FIX] reporte de pedido de ventas.

--- modules/custom_sale_order/report/sale_order.py
+++ modules/custom_sale_order/report/sale_order.py
@@ -19,14 +19,15 @@ from reportlab.lib.utils import ImageReader
 from odoo.addons.report_it.utils import register_fonts
 from datetime import datetime
 # global variables
-wPage,hPage = (595,842)
+#wPage,hPage = (595,842)
+wPage,hPage = A4
 pos_left = 20 # padding
 bottom = 55
 pos = hPage-96 # dynamic vertical position
 wUtil = wPage-2*pos_left # Util width : Real Width - left margins
 middle = wPage/2 # Center page
 pos_right = middle+pos_left
-col_widths0 = [float(i)/100*wUtil for i in (64,12,12,12)] # size of columns (list sum = 100%)
+col_widths0 = [float(i)/100*wUtil for i in ((10,20,11,11) + 4*(12,))] # size of columns (list sum = 100%)
 col_widths = [float(i)/100*wUtil for i in (7,6,5,6,4,6,12,6,6,6,6,6,6,6,6,6)] # size of columns (list sum = 100%)
 
 col_widths1 = [float(i)/100*wUtil for i in (37,25,12)] # size of columns (list sum = 100%)
@@ -37,10 +38,11 @@ register_fonts(['Calibri', 'Calibri-Bold'])
 
 p1 = ParagraphStyle('p1',alignment=TA_CENTER,fontSize=8,fontName="Calibri-Bold")
 p1_1 = ParagraphStyle('p1_1',alignment=TA_LEFT,fontSize=8,fontName="Calibri-Bold")
-p2 = ParagraphStyle('p2',alignment=TA_LEFT,fontSize=7,fontName="Calibri")
-p2_1 = ParagraphStyle('p2_1',alignment=TA_CENTER,fontSize=7,fontName="Calibri")
+p2 = ParagraphStyle('p2',alignment=TA_LEFT,fontSize=8,fontName="Calibri")
+p2_1 = ParagraphStyle('p2_1',alignment=TA_CENTER,fontSize=8,fontName="Calibri")
+p2_2 = ParagraphStyle('p2_2',alignment=TA_LEFT,fontSize=8,fontName="Calibri")
 p3 = ParagraphStyle('p3',alignment=TA_LEFT,fontSize=10,fontName="Calibri")
-p4 = ParagraphStyle('p4',alignment=TA_RIGHT,fontSize=10,fontName="Calibri")
+p4 = ParagraphStyle('p4',alignment=TA_RIGHT,fontSize=8,fontName="Calibri")
 p5 = ParagraphStyle('p5',alignment=TA_RIGHT,fontSize=10,fontName="Calibri-Bold")
 p6 = ParagraphStyle('p6',alignment=TA_LEFT,fontSize=10,fontName="Calibri-Bold")
 p7 = ParagraphStyle('p7',alignment=TA_LEFT,fontSize=11,fontName="Calibri")
@@ -105,59 +107,71 @@ class SaleOrder(models.Model):
 		header_table1(c,pos)
 
 		def header_table2(c,pos):
-			data=[[Paragraph(u"Cliente: %s"%(self.partner_id.name or ' '),p1_1)],]
-			hTable=Table(data,colWidths=340,rowHeights=(15))
+			data=[
+				[Paragraph(u"Cliente: %s"%(self.partner_id.name or ' '),p1_1),'','',''],
+				[Paragraph(u"Contacto: %s"%(self.contact_order_id.name or ' '),p1_1),'','',''],
+				[Paragraph(u"DNI O RUC:",p1_1),Paragraph(u"%s"%(self.partner_id.vat or ' '),p1_1),Paragraph(u"Codigo:",p1_1),Paragraph(u"%s"%(self.partner_id.ref or ' '),p1_1)],
+				[Paragraph(u"Dirección: %s" % (self.partner_id.street or ' '),p1_1),'','',''],
+				[Paragraph(u"Ciudad: %s-%s-%s"%(self.partner_id.state_id.name or ' ',self.partner_id.province_id.name or ' ',self.partner_id.district_id.name or ' '),p1_1),'','',''],
+				[Paragraph(u"Telefono: %s"%(self.partner_id.phone or ' '),p1_1),'','',''],
+			]
+			hTable=Table(data, colWidths=4*(85,), rowHeights=6*(15,))
 			hTable.setStyle(TableStyle([
 				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
+				('BOX',(0,0),(-1,-1),1, colors.black),
+				('SPAN',(0,0),(-1,0)),
+				('SPAN',(0,1),(-1,1)),
+				('SPAN',(0,3),(-1,3)),
+				('SPAN',(0,4),(-1,4)),
+				('SPAN',(0,5),(-1,5)),
 				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-50)
+			hTable.wrapOn(c, 120, 500)
+			hTable.drawOn(c, pos_left, pos-125)
 
-			data=[[Paragraph(u"Contacto: %s"%(self.contact_order_id.name or ' '),p1_1)],]
-			hTable=Table(data,colWidths=340,rowHeights=(15))
-			hTable.setStyle(TableStyle([
-				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
-				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-65)
+			# data=[[Paragraph(u"Contacto: %s"%(self.contact_order_id.name or ' '),p1_1)],]
+			# hTable=Table(data,colWidths=340,rowHeights=(15))
+			# hTable.setStyle(TableStyle([
+			# 	('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+			# 	('BOX',(0,0),(-1,-1),1,colors.black)
+			# 	]))
+			# hTable.wrapOn(c,120,500)
+			# hTable.drawOn(c,pos_left,pos-65)
 
-			data=[[Paragraph(u"DNI O RUC:",p1_1),Paragraph(u"%s"%(self.partner_id.vat or ' '),p1_1),Paragraph(u"Codigo:",p1_1),Paragraph(u"%s"%(self.partner_id.ref or ' '),p1_1)],]
-			hTable=Table(data,colWidths=85,rowHeights=(15))
-			hTable.setStyle(TableStyle([
-				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
-				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-80)
+			# data=[[Paragraph(u"DNI O RUC:",p1_1),Paragraph(u"%s"%(self.partner_id.vat or ' '),p1_1),Paragraph(u"Codigo:",p1_1),Paragraph(u"%s"%(self.partner_id.ref or ' '),p1_1)],]
+			# hTable=Table(data,colWidths=85,rowHeights=(15))
+			# hTable.setStyle(TableStyle([
+			# 	('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+			# 	('BOX',(0,0),(-1,-1),1,colors.black)
+			# 	]))
+			# hTable.wrapOn(c,120,500)
+			# hTable.drawOn(c,pos_left,pos-80)
 
-			data=[[Paragraph(u"Dirección: %s"%(self.partner_id.street or ' '),p1_1)],]
-			hTable=Table(data,colWidths=340,rowHeights=(15))
-			hTable.setStyle(TableStyle([
-				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
-				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-95)
+			# data=[[Paragraph(u"Dirección: %s" % (self.partner_id.street or ' '),p1_1)],]
+			# hTable=Table(data, colWidths=340, rowHeights=(15))
+			# hTable.setStyle(TableStyle([
+			# 	('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+			# 	('BOX',(0,0),(-1,-1),1,colors.black)
+			# 	]))
+			# hTable.wrapOn(c,120,500)
+			# hTable.drawOn(c,pos_left,pos-95)
 
-			data=[[Paragraph(u"Ciudad: %s-%s-%s"%(self.partner_id.state_id.name or ' ',self.partner_id.province_id.name or ' ',self.partner_id.district_id.name or ' '),p1_1)],]
-			hTable=Table(data,colWidths=340,rowHeights=(15))
-			hTable.setStyle(TableStyle([
-				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
-				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-110)
+			# data=[[Paragraph(u"Ciudad: %s-%s-%s"%(self.partner_id.state_id.name or ' ',self.partner_id.province_id.name or ' ',self.partner_id.district_id.name or ' '),p1_1)],]
+			# hTable=Table(data,colWidths=340,rowHeights=(15))
+			# hTable.setStyle(TableStyle([
+			# 	('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+			# 	('BOX',(0,0),(-1,-1),1,colors.black)
+			# 	]))
+			# hTable.wrapOn(c,120,500)
+			# hTable.drawOn(c,pos_left,pos-110)
 
-			data=[[Paragraph(u"Telefono: %s"%(self.partner_id.phone or ' '),p1_1)],]
-			hTable=Table(data,colWidths=340,rowHeights=(15))
-			hTable.setStyle(TableStyle([
-				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
-				('BOX',(0,0),(-1,-1),1,colors.black)
-				]))
-			hTable.wrapOn(c,120,500)
-			hTable.drawOn(c,pos_left,pos-125)
+			# data=[[Paragraph(u"Telefono: %s"%(self.partner_id.phone or ' '),p1_1)],]
+			# hTable=Table(data,colWidths=340,rowHeights=(15))
+			# hTable.setStyle(TableStyle([
+			# 	('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+			# 	('BOX',(0,0),(-1,-1),1,colors.black)
+			# 	]))
+			# hTable.wrapOn(c,120,500)
+			# hTable.drawOn(c,pos_left,pos-125)
 
 
 			data= [['Forma de Pago:'],
@@ -222,7 +236,7 @@ class SaleOrder(models.Model):
 			hTable.wrapOn(c,120,500)
 			hTable.drawOn(c,466,pos-65)
 
-			data= [['Codigo:'],
+			data= [[u'Código:'],
 			['%s'%(self.user_id.partner_id.ref or ' ')]]
 			hTable=Table(data,colWidths=106,rowHeights=(15))
 			hTable.setStyle(TableStyle([
@@ -251,9 +265,9 @@ class SaleOrder(models.Model):
 		header_table2(c,pos)
 
 
-		def header_table3(c,pos):
-			data=[[Paragraph(u"Codigo.",p1),Paragraph(u"Item.",p1),Paragraph(u"Item.",p1),Paragraph(u"Bodega U.M.",p1),Paragraph(u"Cantidad",p1),Paragraph(u"Precio Unit.",p1),Paragraph(u"Dscto",p1),Paragraph(u"Subtotal",p1),Paragraph(u"IVA %",p1)],]
-			Table_lines=Table(data,colWidths=61,rowHeights=(20))
+		def header_table3(c, pos):
+			data=[[Paragraph(u"Código.",p1),Paragraph(u"Producto",p1),Paragraph(u"Cantidad",p1),Paragraph(u"U.M.",p1),Paragraph(u"Precio Unit.",p1),Paragraph(u"Dscto",p1),Paragraph(u"Subtotal",p1),Paragraph(u"IVA %",p1)],]
+			Table_lines=Table(data,colWidths=col_widths0,rowHeights=(20))
 			Table_lines.setStyle(TableStyle([
 				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
 				('SPAN',(2,-1),(1,-1)),
@@ -268,22 +282,36 @@ class SaleOrder(models.Model):
 		# results = self._cr.dictfetchall()
 		#print(results)
 		lista =[]
-		precios_s_descuento =[]
+		#precios_s_descuento =[]
+		total_discount = 0.0
+		total_bruto = 0.0
+		fmt_dec = lambda float_n: ' {:,.2f}'.format(Decimal("%0.2f" % float_n or 0.0))
+
 		for line in self.order_line:
 			codigo_producto   = str(line.product_id.default_code or ' ')
 			producto   = str(line.product_id.name or ' ')
-			cantidad   = str(line.product_uom_qty or ' ')
-			precio   = str(line.price_unit or ' ')
-			descuento   =  str( '% ' +str(line.discount) or ' ')
-			suntotal   = str(line.price_subtotal or ' ')
+			cantidad   = fmt_dec(line.product_uom_qty)
+			precio   = fmt_dec(line.price_unit)
+			descuento   =  str( '% ' + fmt_dec(line.discount))
+			suntotal   = fmt_dec(line.price_subtotal)
 
 
-			iva   =  str('% '+str(line.tax_id.amount) or ' ')
+			iva   =  str('% '+fmt_dec(line.tax_id.amount))
+			amount_b = line.tax_id.compute_all(line.price_unit, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)['total_excluded']# subtotal sin descuento
 
-			precios_s_descuento.append(line.product_uom_qty*line.price_unit)
+			total_discount += amount_b - line.price_subtotal if line.discount > 0.0 else 0.0
+			total_bruto += amount_b
 
-			data = [[Paragraph(codigo_producto,p2_1),Paragraph(producto,p2_1),Paragraph(producto,p2_1),Paragraph(self.warehouse_id.name,p2_1),Paragraph(cantidad,p2_1),Paragraph(precio,p2_1),Paragraph(descuento,p2_1),Paragraph(suntotal,p2_1),Paragraph(iva,p2_1)],]
-			t=Table(data,colWidths=61,rowHeights=(20))
+			data = [[
+				Paragraph(codigo_producto,p2_2),
+				Paragraph(producto,p2_2),
+				Paragraph(cantidad,p4),
+				Paragraph(line.product_uom.name,p2_2),
+				Paragraph(precio,p4),
+				Paragraph(descuento,p4),
+				Paragraph(suntotal,p4),
+				Paragraph(iva,p4)],]
+			t=Table(data, colWidths=col_widths0)
 			t.setStyle(TableStyle([
 			('VALIGN',(0,0),(-1,-1),'TOP'),
 			('SPAN',(2,-1),(1,-1)),
@@ -319,11 +347,18 @@ class SaleOrder(models.Model):
 			Table_lines.wrapOn(c,120,500)
 			Table_lines.drawOn(c,pos_left,(pos-170)-70)
 
-			descuento = round((sum(precios_s_descuento)-float(self.amount_untaxed)),2)
+			fmt_dec_curr = lambda float_n: self.currency_id.symbol + ' {:,.2f}'.format(Decimal("%0.2f" % float_n or 0.0))
 
-			moneda=str(self.currency_id.symbol)
-
-			data=[[Paragraph(moneda + str(round(sum(precios_s_descuento),2)) or '',p2_1),Paragraph(moneda + str(descuento)or '',p2_1),Paragraph(moneda + str(self.amount_untaxed),p2_1),Paragraph(moneda + str(self.amount_tax),p2_1),Paragraph(moneda + str(self.amount_total),p2_1)],]
+			data = [
+					[
+					Paragraph(fmt_dec_curr(total_bruto),p2_1), 
+					Paragraph(fmt_dec_curr(total_discount), p2_1),
+					Paragraph( fmt_dec_curr(self.amount_untaxed),p2_1),
+					Paragraph(fmt_dec_curr(self.amount_tax) ,p2_1),
+					Paragraph(fmt_dec_curr(self.amount_total), p2_1)
+					],
+				]
+			
 			Table_lines=Table(data,colWidths=110.4,rowHeights=(15))
 			Table_lines.setStyle(TableStyle([
 				('VALIGN',(0,0),(-1,-1),'MIDDLE'),
@@ -381,6 +416,9 @@ class SaleOrder(models.Model):
 #------------------------------------------------------------------------
 #------------------------------------------------------------------------
 
+	# ========
+	# WTF ????
+	# FIXME TODO Refactorizar ésta huevada, odoo ya tiene métodos para hacer ésto!!!!
 
 	def convierte_cifra(self,numero,sw):
 		lista_centana = ["",("CIEN","CIENTO"),"DOSCIENTOS","TRESCIENTOS","CUATROCIENTOS","QUINIENTOS","SEISCIENTOS","SETECIENTOS","OCHOCIENTOS","NOVECIENTOS"]
